f82e16a208787437dc531c578e0ee2a6eb794380
Christina <christina.hsu.ca@gmail.com>
2019-01-14 20:14:54 +0000

    modified http.js & updated with another file called http2.js for question2

--- http.js
+++ http.js
@@ -4,15 +4,13 @@ function getAndPrintHTMLChunks(){
         host: 'sytantris.github.io',
         path: '/http-examples/step1.html'
     };
-    var chunk = [];
-    var http = require('http');
-    http.get(requestOptions, function(response){
+
+    var https = require('https');
+    https.get(requestOptions, function(response){
         response.setEncoding('utf-8');
 
         response.on('data', function(data) {
             console.log('Received. Length:', data.length);
-            chunk += data + '\n';
-            console.log(chunk);
         })
 
         response.on('end', function() {
