ab4a5eaa70adbf50c31e1476535695831ce0b8bd
Christina <christina.hsu.ca@gmail.com>
2019-01-14 21:10:56 +0000

    creating module by using module.export

---
+++ http-functions.js
@@ -0,0 +1,17 @@
+module.exports = function getHTML (options, callback) {
+    var https = require('https');
+    var chunks = '';
+    https.get(options, function (data) {
+        data.setEncoding('utf-8');
+        data.on('data', function(data) {
+            chunks += data + '\n';
+            console.log(chunks); 
+        })
+
+        callback(chunks);
+
+        data.on('end', function () {
+            console.log('completed process');
+        })
+    })
+}
