c2930845e27c0c1281711a3e413691e654a5d113
Christina <christina.hsu.ca@gmail.com>
2019-01-14 21:38:49 +0000

    changing the format of the content

--- http-functions.js
+++ http-functions.js
@@ -5,13 +5,14 @@ module.exports = function getHTML (options, callback) {
         data.setEncoding('utf-8');
         data.on('data', function(data) {
             chunks += data + '\n';
-            console.log(chunks); 
+            console.log(chunks);
         })
 
-        callback(chunks);
+
 
         data.on('end', function () {
-            console.log('completed process');
+            //console.log('completed process');
+            callback(chunks);
         })
     })
 }
